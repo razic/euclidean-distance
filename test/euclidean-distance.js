31d1c6d5147de09744280a212f10de1f606445b0
razic <razic@viralkitty.com>
2013-05-20 01:29:23 +0700

    add test folder

---
+++ test/euclidean-distance.js
@@ -0,0 +1,8 @@
+var assert = require('assert'),
+    euclideanDistance = require('euclidean-distance');
+
+describe('euclideanDistance', function() {
+  it('should be a function', function() {
+    assert(typeof euclideanDistance === 'function');
+  });
+})

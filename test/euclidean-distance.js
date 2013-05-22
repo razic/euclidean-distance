741f13961b4c5cbadf34da4d4ddb06f40c396a0e
razic <razic@viralkitty.com>
2013-05-22 17:05:06 +0700

    added ability to calculate from any dimension

--- test/euclidean-distance.js
+++ test/euclidean-distance.js
@@ -3,116 +3,75 @@ var assert = require('assert'),
     x1 = 5,
     y1 = 0,
     z1 = -9.2,
+    n1 = -3
     x2 = -5.99,
     y2 = -10.02,
     z2 = 2,
+    n2 = 7.1,
+    distance1D = 10.99,
     distance2D = 14.872138380206122,
-    distance3D = 18.6177469098707;
+    distance3D = 18.6177469098707,
+    distance4D = 21.180899414330828;
 
-describe('euclideanDistance(x1, y1, x2, y2)', function() {
-  describe('when there are four arguments that are numbers', function() {
-    it('should calculate properly', function() {
-      var result = euclideanDistance(x1, y1, x2, y2);
+describe('euclideanDistance(x1, y1, ... )', function() {
+  it('should calculate properly', function() {
+    var result1D = euclideanDistance(x1, x2),
+        result2D = euclideanDistance(x1, y1, x2, y2),
+        result3D = euclideanDistance(x1, y1, z1, x2, y2, z2),
+        result4D = euclideanDistance(x1, y1, z1, n1, x2, y2, z2, n2);
 
-      assert(result === distance2D);
-    });
+    assert(result1D === distance1D);
+    assert(result2D === distance2D);
+    assert(result3D === distance3D);
+    assert(result4D === distance4D);
   });
-});
-
-describe('euclideanDistance(x1, y1, z1 x2, y2, z2)', function() {
-  describe('when there are six arguments that are numbers', function() {
-    it('should calculate properly', function() {
-      var result = euclideanDistance(x1, y1, z1, x2, y2, z2);
-
-      assert(result === distance3D);
-    });
-  });
-});
-
-describe('euclideanDistance({ x: x1, y: y1 }, { x: x2, y: y2 })', function() {
-  describe(
-    'when there are two arguments that are objects, each containing x and y \
-    properties',
-    function() {
-      it('should calculate properly', function() {
-        var result = euclideanDistance({ x: x1, y: y1 }, { x: x2, y: y2 });
-
-        assert(result === distance2D);
-      });
-    }
-  );
 });
 
 describe(
-  'euclideanDistance({ x: x1, y: y1, z: z1 }, { x: x2, y: y2, z2 })',
+  'euclideanDistance({ x: x1, y: y1, ... }, { x: x2, y: y2, ... })',
   function() {
-    describe(
-      'when there are two arguments that are objects, each containing x, y and \
-      z properties',
-      function() {
-        it('should calculate properly', function() {
-          var result = euclideanDistance(
-            { x: x1, y: y1, z: z1 },
-            { x: x2, y: y2, z: z2 }
+    it('should calculate properly', function() {
+      var result1D = euclideanDistance({ x: x1 }, { x: x2 }),
+          result2D = euclideanDistance({ x: x1, y: y1 }, { x: x2, y: y2 }),
+          result3D = euclideanDistance(
+            { x: x1, y: y1, z: z1 }, { x: x2, y: y2, z: z2 }
+          ),
+          result4D = euclideanDistance(
+            { x: x1, y: y1, z: z1, n: n1 }, { x: x2, y: y2, z: z2, n: n2 }
           );
 
-          assert(result === distance3D);
-        });
-      }
-    );
+      assert(result1D === distance1D);
+      assert(result2D === distance2D);
+      assert(result3D === distance3D);
+      assert(result4D === distance4D);
+    });
   }
 );
 
-describe('euclideanDistance([x1, y1], [x2, y2])', function() {
-  describe(
-    'when there are two arguments that are arrays, each containing two \
-    numbers',
-    function() {
-      it('should calculate properly', function() {
-        var result = euclideanDistance([x1, y1], [x2, y2]);
+describe('euclideanDistance([x1, y1, ... ], [x2, y2 ... ])', function() {
+  it('should calculate properly', function() {
+    var result1D = euclideanDistance([x1], [x2]),
+        result2D = euclideanDistance([x1, y1], [x2, y2]),
+        result3D = euclideanDistance([x1, y1, z1], [x2, y2, z2]),
+        result4D = euclideanDistance([x1, y1, z1, n1], [x2, y2, z2, n2]);
 
-        assert(result === distance2D);
-      });
-    }
-  );
+    assert(result1D === distance1D);
+    assert(result2D === distance2D);
+    assert(result3D === distance3D);
+    assert(result4D === distance4D);
+  });
 });
 
-describe('euclideanDistance([x1, y1, z1], [x2, y2, z2])', function() {
-  describe(
-    'when there are two arguments that are arrays, each containing three \
-    numbers',
-    function() {
-      it('should calculate properly', function() {
-        var result = euclideanDistance([x1, y1, z1], [x2, y2, z2]);
+describe('euclideanDistance([x1, y1, ... ])', function() {
+  it('should calculate properly', function() {
+    var result1D = euclideanDistance([x1, x2]),
+        result2D = euclideanDistance([x1, y1, x2, y2]),
+        result3D = euclideanDistance([x1, y1, z1, x2, y2, z2]),
+        result4D = euclideanDistance([x1, y1, z1, n1, x2, y2, z2, n2]);
 
-        assert(result === distance3D);
-      });
-    }
-  );
-});
-
-describe('euclideanDistance([x1, y1, x2, y2])', function() {
-  describe(
-    'when there is one argument that is an array, containing four numbers',
-    function() {
-      it('should calculate properly', function() {
-        var result = euclideanDistance([x1, y1, x2, y2]);
-
-        assert(result === distance2D);
-      });
-    }
-  );
-});
-
-describe('euclideanDistance([x1, y1, z1 x2, y2, z2])', function() {
-  describe(
-    'when there is one argument that is an array, containing six numbers',
-    function() {
-      it('should calculate properly', function() {
-        var result = euclideanDistance([x1, y1, z1, x2, y2, z2]);
-
-        assert(result === distance3D);
-      });
-    }
-  );
+    assert(result1D === distance1D);
+    assert(result2D === distance2D);
+    assert(result3D === distance3D);
+    assert(result4D === distance4D);
+  });
 });
